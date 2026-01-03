d8b7664a54af6bfd4dd54e04b044409ef24006fc
Gonzalo Casanova <gjcasanova@teco.com.ar>
2026-01-03 17:57:28 -0300

    create user use case

---
+++ src/modules/users/app/usecases/UserCreate.ts
@@ -0,0 +1,20 @@
+import { PgUserRepository } from "../../infra/repository/users.repository";
+import {NonIdUser, UserRepository, UserUseCase } from "../../interfaces/users.interfaces";
+import { randomUUID, hash } from 'crypto'
+
+export class UserCreate implements UserUseCase {
+  private readonly repo: UserRepository
+  
+  constructor() {
+    this.repo = new PgUserRepository()
+  }
+
+  async exec(data: NonIdUser): Promise<void> {
+    return await this.repo.createUser({
+      ...data,
+      id: randomUUID(),
+      password: hash("sha512", data.password),
+      updatedAt: new Date()
+    })
+  }
+}
