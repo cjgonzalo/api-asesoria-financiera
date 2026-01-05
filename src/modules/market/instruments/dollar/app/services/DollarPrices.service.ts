67f6ebcbd02e70f9d5c5e7e396305500092ecd68
Gonzalo Casanova <gjcasanova@teco.com.ar>
2026-01-05 03:35:58 -0300

    dollar service

---
+++ src/modules/market/instruments/dollar/app/services/DollarPrices.service.ts
@@ -0,0 +1,20 @@
+import { UUID } from "crypto"
+import { FindCurrentPrices } from "../usecases/FindCurrentPrices"
+import { FindPricesByHouse } from "../usecases/FindPricesByHouse"
+import { DollarPrice } from "../../interfaces/DollarInterfaces"
+import { SaveDollarPrice } from "../usecases/SaveDollarPrice"
+
+
+export const DollarPricesService = {
+  async findCurrentPrices() {
+    return await new FindCurrentPrices().exec()
+  },
+
+  async findPircesByHouse(house: UUID) {
+    return await new FindPricesByHouse().exec(house)
+  },
+
+  async saveCurrentPrices() {
+    return await new SaveDollarPrice().exec()
+  }
+}
