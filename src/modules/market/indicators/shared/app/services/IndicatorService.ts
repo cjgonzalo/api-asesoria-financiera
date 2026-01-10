87eb8a3a86acb0b5bf8f23da792cf69eac4ac748
Gonzalo Casanova <gjcasanova@teco.com.ar>
2026-01-10 02:14:45 -0300

    indicators service

---
+++ src/modules/market/indicators/shared/app/services/IndicatorService.ts
@@ -0,0 +1,33 @@
+import { IndicatorObservation } from "../../interfaces/IndicatorInterfaces"
+import { FindAllIndicators } from "../usecases/FindAllIndicators"
+import { FindAllObservations } from "../usecases/FindAllObservations"
+import { FindIndicatorById } from "../usecases/FindIndicatorById"
+import { FindIndicatorByName } from "../usecases/FindIndicatorByName"
+import { FindObservationsById } from "../usecases/FindObservationsById"
+import { SaveObservations } from "../usecases/SaveObservations"
+
+export const IndicatorService = {
+  async getAllIndicators() {
+    return await new FindAllIndicators().exec()
+  },
+
+  async getIndicatorById(id: string) {
+    return await new FindIndicatorById().exec(id)
+  },
+
+  async getIndicatorByName(name: string) {
+    return await new FindIndicatorByName().exec(name)
+  },
+
+  async getAllObservations() {
+    return await new FindAllObservations().exec()
+  },
+
+  async getObservationsById(id: string) {
+    return await new FindObservationsById().exec(id)
+  },
+
+  async saveObservations(obs: IndicatorObservation[]) {
+    return await new SaveObservations().exec(obs)
+  },
+}
