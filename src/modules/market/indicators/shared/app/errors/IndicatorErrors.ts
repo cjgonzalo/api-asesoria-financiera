c3622b0a5091eda3625fab036cc09587134f93bc
Gonzalo Casanova <gjcasanova@teco.com.ar>
2026-01-10 04:29:54 -0300

    new observation not found error

--- src/modules/market/indicators/shared/app/errors/IndicatorErrors.ts
+++ src/modules/market/indicators/shared/app/errors/IndicatorErrors.ts
@@ -5,6 +5,11 @@ export const INDICATOR_NOT_FOUND: HttpError = {
   message: "Indicator not found"
 }
 
+export const OBSERVATIONS_NOT_FOUND: HttpError = {
+  httpCode: 400,
+  message: "Observations not found"
+}
+
 export const SAVE_OBSERVATION_ERROR: HttpError = {
   httpCode: 400,
   message: "There was an error saving the observation"
