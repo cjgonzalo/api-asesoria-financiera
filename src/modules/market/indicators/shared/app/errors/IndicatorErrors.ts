14fa9785a58383fee588c3f9339e30a28722d22c
Gonzalo Casanova <gjcasanova@teco.com.ar>
2026-01-10 02:05:50 -0300

    indicator errors

---
+++ src/modules/market/indicators/shared/app/errors/IndicatorErrors.ts
@@ -0,0 +1,11 @@
+import { HttpError } from "../../../../../../errors/CustomError";
+
+export const INDICATOR_NOT_FOUND: HttpError = {
+  httpCode: 400,
+  message: "Indicator not found"
+}
+
+export const SAVE_OBSERVATION_ERROR: HttpError = {
+  httpCode: 400,
+  message: "There was an error saving the observation"
+}
