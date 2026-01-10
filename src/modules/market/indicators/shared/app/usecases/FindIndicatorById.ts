bfa441ad283391e506c860005ebf8388628a9a95
Gonzalo Casanova <gjcasanova@teco.com.ar>
2026-01-10 02:07:30 -0300

    find indicator by id use case

---
+++ src/modules/market/indicators/shared/app/usecases/FindIndicatorById.ts
@@ -0,0 +1,22 @@
+import { CustomError } from "../../../../../../errors/CustomError";
+import { PgIndicatorRepository } from "../../infra/repository/IndicatorRepository";
+import { Indicator, IndicatorRepository, IndicatorUseCase } from "../../interfaces/IndicatorInterfaces";
+import { INDICATOR_NOT_FOUND } from "../errors/IndicatorErrors";
+
+export class FindIndicatorById implements IndicatorUseCase {
+  private readonly repo: IndicatorRepository
+
+  constructor() {
+    this.repo = new PgIndicatorRepository()
+  }
+
+  async exec(id: string): Promise<Indicator> {
+    const indicator = await this.repo.findIndicatorById(id)
+    
+    if(!indicator) {
+      throw new CustomError(INDICATOR_NOT_FOUND)
+    }
+
+    return indicator 
+  }
+}
